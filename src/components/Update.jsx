0df9fb3e2282396671169f3010a30536b73e8f59
Rakibul Islam <mdrakib.5627@gmail.com>
2023-10-14 17:18:48 +0600

    first commit

---
+++ src/components/Update.jsx
@@ -0,0 +1,47 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const Update = () => {
+
+    const loadedUser = useLoaderData();
+
+    const handleUpdate = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        console.log(name ,email)
+        const updatedUser = { name, email };
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.insertedId) {
+          alert('Users updated successfully');
+          form.reset();
+        }
+      })
+    }
+
+    return (
+        <div>
+            <p>{loadedUser.name}</p>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={loadedUser?.name} id="" />
+                <br />
+                <input type="email" name="email" defaultValue={loadedUser?.email} id="" />
+                <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
